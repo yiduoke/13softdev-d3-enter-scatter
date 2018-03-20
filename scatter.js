6935214a253fbd62bf9807d9250d1820554510b4
Yiduo Ke <yiduoke822@jhs185.org>
2018-03-19 20:26:39 -0400

    DONEEEE

--- scatter.js
+++ scatter.js
@@ -7,8 +7,34 @@ for (i = 0; i < 47; i++){
 }
 
 console.log(all)
-
+// adding the plots in
 var container = d3.select("svg");
 var dots = container.selectAll("circle").data(all).enter();
-dots.append("circle").attr("cx", function(d){return (d[0] - 1950) * 10 + 10}).attr("cy", function(d){return Math.round((80 - d[1]) * 6.25) + 20}).attr("r", 3);
+// dots.append("circle").attr("cx", function(d){return (d[0] - 1950) * 10 + 20;}).attr("cy", function(d){return Math.round((80 - d[1]) * 6 + 20);}).attr("r", 3);
+dots.append("circle").attr("cx", function(d){return (d[0] - 1950) * 10 + 50;}).attr("cy", function(d){return Math.round((80 - d[1]) * 6 + 20);}).attr("r", 3);
 
+// axis
+var xscale = d3.scaleLinear().domain([1950, 1996]).range([50, 510]);
+var yscale = d3.scaleLinear().domain([0, 80]).range([500, 20]);
+
+var x_axis = d3.axisBottom().scale(xscale).tickFormat(d3.format("d")); // to get rid of the comma for the years
+var y_axis = d3.axisLeft().scale(yscale);
+
+//Draw axes and move them
+container.append("g").attr("transform", "translate(0, 500)").call(x_axis);
+container.append("g").attr("transform", "translate(50, 0)").call(y_axis);
+
+container.append("text")
+.attr("transform", "rotate(-90)")
+.attr("x", -280)
+.attr("y", 0)
+.attr("dy", "1em")
+.style("text-anchor", "middle")
+.text("Life Expectancy (Years)");
+
+container.append("text")
+.attr("x", 300)
+.attr("y", 520)
+.attr("dy", "1em")
+.style("text-anchor", "middle")
+.text("Year");
